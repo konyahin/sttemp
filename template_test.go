a15b1eef478c103151fde1a6f8b71be1515b8bcd
Anton Konyahin <me@konyahin.xyz>
2026-01-03 21:59:52 +0300

    rewrite tests with anonimous structs

--- template_test.go
+++ template_test.go
@@ -5,87 +5,167 @@ import (
 	"testing"
 )
 
-type VariableTestCase struct {
-	content   []byte
-	variables []string
-}
-
-func newVariableTestCase(content string, vars ...string) *VariableTestCase {
-	testCase := new(VariableTestCase)
-	testCase.content = []byte(content)
-	testCase.variables = vars
-	return testCase
-}
-
-type FillTemplateTestCase struct {
-	content string
-	result  string
-	values  map[string]string
-}
-
-func newFillTemplateTestCase(content string, result string, values map[string]string) *FillTemplateTestCase {
-	testCase := new(FillTemplateTestCase)
-	testCase.content = content
-	testCase.result = result
-	testCase.values = values
-	return testCase
-}
-
 func TestFindVariables(t *testing.T) {
-	testCases := []*VariableTestCase{
-		newVariableTestCase(""),
-		newVariableTestCase("without variables"),
-		newVariableTestCase("variable {A}", "A"),
-		newVariableTestCase("variable {A} and {A}", "A"),
-		newVariableTestCase("empty variable {}"),
-		newVariableTestCase("line break {A\n}"),
-		newVariableTestCase("escaping \\{A}"),
-		newVariableTestCase("fake escaping \\[A]"),
-		newVariableTestCase("few variables {A}, {A B}, {A B C}", "A", "A B", "A B C"),
+	testCases := []struct {
+		name      string
+		content   string
+		variables []string
+	}{
+		{
+			name:      "empty string",
+			content:   "",
+			variables: []string{},
+		},
+		{
+			name:      "without variables",
+			content:   "just\nsome text",
+			variables: []string{},
+		},
+		{
+			name:      "single variable",
+			content:   "variable {A}",
+			variables: []string{"A"},
+		},
+		{
+			name:      "same variable twice",
+			content:   "variable {A} and {A}",
+			variables: []string{"A"},
+		},
+		{
+			name:      "empty variable",
+			content:   "just\n\tsome text {} another text",
+			variables: []string{},
+		},
+		{
+			name:      "line break in variable name",
+			content:   "line break {A\n}",
+			variables: []string{},
+		},
+		{
+			name:      "variable escaping",
+			content:   "escaping \\{A} {B}",
+			variables: []string{"B"},
+		},
+		{
+			name:      "escaping without variable",
+			content:   "fake escaping \\[A] {B}",
+			variables: []string{"B"},
+		},
+		{
+			name:      "few variables",
+			content:   "one {A}, two{A B}, three {A B C}",
+			variables: []string{"A", "A B", "A B C"},
+		},
 	}
 
-	for _, testCase := range testCases {
-		template := NewTemplate("", testCase.content)
-		if !slices.Equal(template.Variables, testCase.variables) {
-			t.Fatalf("We should get %#v, but got %#v", testCase.variables, template.Variables)
-		}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			template := NewTemplate("", []byte(tt.content))
+			if !slices.Equal(template.Variables, tt.variables) {
+				t.Fatalf("We should get %#v, but got %#v", tt.variables, template.Variables)
+			}
+		})
 	}
 }
 
 func TestFillTemplate(t *testing.T) {
-	testCases := []*FillTemplateTestCase{
-		newFillTemplateTestCase("Test", "Test", nil),
-		newFillTemplateTestCase("Test {A}", "Test ", nil),
-		newFillTemplateTestCase("Test", "Test", map[string]string{
-			"A": "B",
-		}),
-		newFillTemplateTestCase("Test {A}", "Test B", map[string]string{
-			"A": "B",
-		}),
-		newFillTemplateTestCase("Test {A} rest", "Test B rest", map[string]string{
-			"A": "B",
-			"B": "C",
-		}),
-		newFillTemplateTestCase("Test {A} rest {B}", "Test 1 rest 2", map[string]string{
-			"A": "1",
-			"B": "2",
-		}),
-		newFillTemplateTestCase("Test {A\n} rest {B} and {B C}", "Test {A\n} rest SOME TEXT and ANOTHER TEXT", map[string]string{
-			"A":   "WITHOUT",
-			"B":   "SOME TEXT",
-			"B C": "ANOTHER TEXT",
-		}),
-		newFillTemplateTestCase("Test {A} rest {B}!", "Test  rest SOME TEXT!", map[string]string{
-			"B": "SOME TEXT",
-		}),
+	testCases := []struct {
+		name    string
+		content string
+		result  string
+		values  map[string]string
+	}{
+		{
+			name:    "without variables",
+			content: "Test\ntest",
+			result:  "Test\ntest",
+			values:  map[string]string{},
+		},
+		{
+			name:    "empty variable",
+			content: "Test {A}",
+			result:  "Test ",
+			values:  map[string]string{},
+		},
+		{
+			name:    "empty variable with values",
+			content: "Test {C}",
+			result:  "Test ",
+			values: map[string]string{
+				"A": "A",
+				"B": "B",
+			},
+		},
+		{
+			name:    "one variable",
+			content: "Test {A}",
+			result:  "Test Var",
+			values: map[string]string{
+				"A": "Var",
+			},
+		},
+		{
+			name:    "one variable and some text",
+			content: "Test {A} another text",
+			result:  "Test Var another text",
+			values: map[string]string{
+				"A": "Var",
+			},
+		},
+		{
+			name:    "two variables",
+			content: "Test {A} rest {B}",
+			result:  "Test 1 rest 2\n3",
+			values: map[string]string{
+				"A": "1",
+				"B": "2\n3",
+			},
+		},
+		{
+			name:    "new line in a name",
+			content: "Test {A\n} rest {B} and {B C}",
+			result:  "Test {A\n} rest SOME TEXT and ANOTHER TEXT",
+			values: map[string]string{
+				"A":   "WITHOUT",
+				"B":   "SOME TEXT",
+				"B C": "ANOTHER TEXT",
+			},
+		},
+		{
+			name:    "some variables are empty",
+			content: "Test {A} rest {B}!",
+			result:  "Test  rest SOME TEXT!",
+			values: map[string]string{
+				"B": "SOME TEXT",
+			},
+		},
+		{
+			name:    "variable escaping",
+			content: "Test \\{A} rest {B}!",
+			result:  "Test {A} rest SOME TEXT!",
+			values: map[string]string{
+				"A": "WITHOUT",
+				"B": "SOME TEXT",
+			},
+		},
+		{
+			name:    "escaping without variable",
+			content: "Test \\[A] rest {B}!",
+			result:  "Test \\[A] rest SOME TEXT!",
+			values: map[string]string{
+				"A": "WITHOUT",
+				"B": "SOME TEXT",
+			},
+		},
 	}
 
-	for _, testCase := range testCases {
-		template := NewTemplate("", []byte(testCase.content))
-		result := template.fillTemplate(testCase.values)
-		if result != testCase.result {
-			t.Errorf("Test case: %#v", testCase)
-			t.Fatalf("We should get %#v, but got %#v", testCase.result, result)
-		}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			template := NewTemplate("", []byte(tt.content))
+			result := template.fillTemplate(tt.values)
+			if result != tt.result {
+				t.Fatalf("We should get %#v, but got %#v", tt.result, result)
+			}
+		})
 	}
 }
