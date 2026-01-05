0df516f91f6f9ef5f4a57f6ec94bdf42ad9e74f0
Anton Konyahin <me@konyahin.xyz>
2026-01-05 23:31:20 +0300

    move template's directory logic to storage struct

--- main.go
+++ main.go
@@ -3,84 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
-const defaultTemplateDir = ".local/share/sttemp"
-
-func getDefaultBaseDir() (string, error) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-
-	return filepath.Join(home, defaultTemplateDir), nil
-}
-
-func getBaseDir(baseDir string) (string, error) {
-	if baseDir == "" {
-		return getDefaultBaseDir()
-	}
-
-	templatesPath, err := filepath.Abs(baseDir)
-	if err != nil {
-		return "", err
-	}
-
-	return templatesPath, nil
-}
-
-func findTemplateFiles(baseDir string) ([]*TemplateFile, error) {
-	var templateFiles []*TemplateFile
-	err := filepath.WalkDir(baseDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			if os.IsPermission(err) {
-				return nil
-			}
-			return err
-		}
-
-		if d.IsDir() && strings.HasPrefix(d.Name(), ".") {
-			return fs.SkipDir
-		}
-
-		if !d.IsDir() {
-			templateFiles = append(templateFiles, NewTemplateFile(path, baseDir))
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return templateFiles, nil
-}
-
 func main() {
-	path := flag.String("C", "", "template's directory (by default is ~/"+defaultTemplateDir+")")
+	path := flag.String("C", "", "template's directory (by default is ~/"+GetDefaultTemplateDir()+")")
 	outputFileName := flag.String("o", "", "output file name")
 	defaultName := flag.Bool("d", false, "use default name for template")
+
 	flag.Parse()
-
-	baseDir, err := getBaseDir(*path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	templateFiles, err := findTemplateFiles(baseDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	names := flag.Args()
+
+	storage, err := NewStorage(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if len(names) > 0 {
 		var file *os.File = os.Stdout
+
 		if !(*defaultName) && *outputFileName != "" {
 			file, err = os.Create(*outputFileName)
 			if err != nil {
@@ -88,40 +30,41 @@ func main() {
 			}
 			defer file.Close()
 		}
-		for _, name := range names {
-			for _, templateFile := range templateFiles {
-				if name != templateFile.Name {
-					continue
-				}
 
-				content, err := os.ReadFile(templateFile.Path)
+		for _, name := range names {
+			templateFile, ok := storage.templates[name]
+			if !ok {
+				log.Println("Can't find template", name)
+				continue
+			}
+
+			content, err := os.ReadFile(templateFile.Path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			template := NewTemplate(&templateFile, content)
+			values := make(map[string]string)
+			for _, variable := range template.Variables {
+				values[variable] = os.Getenv(variable)
+			}
+			if *defaultName {
+				if template.Filename == "" {
+					log.Fatalf("Template %s has no default name, but -d flag was set", template.Name)
+				}
+				file, err = os.Create(template.Filename)
 				if err != nil {
 					log.Fatal(err)
 				}
-
-				template := NewTemplate(templateFile, content)
-				values := make(map[string]string)
-				for _, variable := range template.Variables {
-					values[variable] = os.Getenv(variable)
-				}
-				if *defaultName {
-					if template.Filename == "" {
-						log.Fatalf("Template %s has no default name, but -d flag was set", template.Name)
-					}
-					file, err = os.Create(template.Filename)
-					if err != nil {
-						log.Fatal(err)
-					}
-					defer file.Close()
-				}
-				fmt.Fprint(file, template.fillTemplate(values))
+				defer file.Close()
 			}
+			fmt.Fprint(file, template.fillTemplate(values))
 		}
 		return
 	}
 
 	// if no args, print all template names
-	for _, templateFile := range templateFiles {
+	for _, templateFile := range storage.templates {
 		fmt.Println(templateFile)
 	}
 }
---
+++ storage.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Storage represent a directory with all templates
+type Storage struct {
+	path      string
+	templates map[string]TemplateFile
+}
+
+func NewStorage(path string) (*Storage, error) {
+	path, err := getStoragePath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	templateFiles, err := findTemplateFiles(path)
+	if err != nil {
+		return nil, err
+	}
+
+	storage := &Storage{
+		path:      path,
+		templates: templateFiles,
+	}
+	return storage, nil
+}
+
+func GetDefaultTemplateDir() string {
+	return ".local/share/sttemp"
+}
+
+func getStoragePath(path string) (string, error) {
+	if path == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		return filepath.Join(home, GetDefaultTemplateDir()), nil
+	}
+
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func findTemplateFiles(path string) (map[string]TemplateFile, error) {
+	templateFiles := make(map[string]TemplateFile)
+	err := filepath.WalkDir(path, func(filePath string, d fs.DirEntry, err error) error {
+		if err != nil {
+			if os.IsPermission(err) {
+				return nil
+			}
+			return err
+		}
+
+		if d.IsDir() && strings.HasPrefix(d.Name(), ".") {
+			return fs.SkipDir
+		}
+
+		if !d.IsDir() {
+			templateFile := NewTemplateFile(filePath, path)
+			templateFiles[templateFile.Name] = *templateFile
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return templateFiles, nil
+}
